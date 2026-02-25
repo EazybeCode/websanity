c2e4b5fc705d529a33bbd7af9085c6cc02c38312
tech933 <tech@eazybe.com>
2026-02-25 13:05:59 +0530

    Update useWebhooksIntegrationSEOEs.ts

--- hooks/useWebhooksIntegrationSEOEs.ts
+++ hooks/useWebhooksIntegrationSEOEs.ts
@@ -280,8 +280,8 @@ export const useWebhooksIntegrationSEOEs = () => {
           "@type": "AggregateOffer",
           "url": "https://eazybe.com/es/pricing",
           "priceCurrency": "EUR",
-          "lowPrice": 1160,
-          "highPrice": 1960,
+          "lowPrice": 11,
+          "highPrice": 19,
           "offerCount": 5,
           "availability": "https://schema.org/InStock"
         },
@@ -331,8 +331,8 @@ export const useWebhooksIntegrationSEOEs = () => {
           "@type": "AggregateOffer",
           "url": "https://eazybe.com/es/pricing",
           "priceCurrency": "EUR",
-          "lowPrice": 1160,
-          "highPrice": 1960,
+          "lowPrice": 11,
+          "highPrice": 19,
           "offerCount": 5,
           "availability": "https://schema.org/InStock"
         },
