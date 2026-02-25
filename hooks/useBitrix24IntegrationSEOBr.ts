da268d2bf72dde9a78dd3a26c3ac44f0bdec453f
tech933 <tech@eazybe.com>
2026-02-25 13:41:18 +0530

    Update useBitrix24IntegrationSEOBr.ts

--- hooks/useBitrix24IntegrationSEOBr.ts
+++ hooks/useBitrix24IntegrationSEOBr.ts
@@ -278,8 +278,8 @@ export const useBitrix24IntegrationSEOBr = () => {
           "@type": "AggregateOffer",
           "url": "https://eazybe.com/br/pricing",
           "priceCurrency": "BRL",
-          "lowPrice": 92,
-          "highPrice": 112,
+          "lowPrice": 96,
+          "highPrice": 162,
           "offerCount": 5,
           "availability": "https://schema.org/InStock"
         },
@@ -320,8 +320,8 @@ export const useBitrix24IntegrationSEOBr = () => {
           "@type": "AggregateOffer",
           "url": "https://eazybe.com/br/pricing",
           "priceCurrency": "BRL",
-          "lowPrice": 92,
-          "highPrice": 112,
+          "lowPrice": 96,
+          "highPrice": 162,
           "offerCount": 5,
           "availability": "https://schema.org/InStock"
         },
