fa03f389bbb32798ac69620eae95f287397bf0b7
tech933 <tech@eazybe.com>
2026-02-25 13:51:41 +0530

    Update usePipedriveIntegrationSEOEs.ts

--- hooks/usePipedriveIntegrationSEOEs.ts
+++ hooks/usePipedriveIntegrationSEOEs.ts
@@ -280,8 +280,8 @@ export const usePipedriveIntegrationSEOEs = () => {
           "@type": "AggregateOffer",
           "url": "https://eazybe.com/es/pricing",
           "priceCurrency": "EUR",
-          "lowPrice": 11,
-          "highPrice": 19,
+          "lowPrice": 25,
+          "highPrice": 42,
           "offerCount": 5,
           "availability": "https://schema.org/InStock"
         },
@@ -331,8 +331,8 @@ export const usePipedriveIntegrationSEOEs = () => {
           "@type": "AggregateOffer",
           "url": "https://eazybe.com/es/pricing",
           "priceCurrency": "EUR",
-          "lowPrice": 11,
-          "highPrice": 19,
+          "lowPrice": 25,
+          "highPrice": 42,
           "offerCount": 5,
           "availability": "https://schema.org/InStock"
         },
